16b759d3a88ff2d9c0a6bf98c84eff5ab79f847c
Prem Jatin <premjverma12@gmail.com>
2025-10-25 13:19:47 +0530

    edited database.py

--- app/database.py
+++ app/database.py
@@ -4,7 +4,10 @@ from sqlalchemy.orm import sessionmaker
 from app.config import settings
 SQLALCHEMY_DATABASE_URL= f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
-engine=create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"sslmode": "require"}  
+)
 
 SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
