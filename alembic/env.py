dcd80d3630f3f4ea34f4fec9d96b0c7695086f44
Prem Jatin <premjverma12@gmail.com>
2025-10-25 14:21:26 +0530

    edited alembicenv.py

--- alembic/env.py
+++ alembic/env.py
@@ -1,5 +1,5 @@
 from logging.config import fileConfig
-
+from sqlalchemy import create_engine
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from app.models import Base
@@ -42,15 +42,17 @@ def run_migrations_offline() -> None:
 
     """
     url = config.get_main_option("sqlalchemy.url")
-    context.configure(
-        url=url,
-        target_metadata=target_metadata,
-        literal_binds=True,
-        dialect_opts={"paramstyle": "named"},
-    )
+    
+    # Create the engine with this URL
+    connectable = create_engine(url)
 
-    with context.begin_transaction():
-        context.run_migrations()
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection, target_metadata=target_metadata
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
 
 
 def run_migrations_online() -> None:
